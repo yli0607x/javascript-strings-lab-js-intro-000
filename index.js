6ce09a2697ef1f5cf9ed6a383b195779d4e7dbb3
Yu Li <yli0607x@gmail.com>
2018-07-26 01:50:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `Let\'s talk about ${topic}.`;
+=======
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `${3+2}`;
+>>>>>>> 7d92ad71aa82cf111a314adf9ee8b2a46a47294b
